fc9adc061912b8d3f4180e1a66fb5b140df125da
joshmun <joshuamun@gmail.com>
2018-01-18 16:52:13 -0800

    prepare for deploy for heroku, finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
-require('./services/passport');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // this doesn't make sense to me
 // is it passing app object into the function? how does it give app as a parameter
